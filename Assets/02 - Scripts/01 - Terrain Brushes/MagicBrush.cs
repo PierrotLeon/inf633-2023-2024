e588c978df9af34f6ae86fa4b9d533eba36639b5
pedroandrade_jp@hotmail.com <pedroandradejps@gmail.com>
2023-09-19 23:58:09 +0200

    Add Magic Brush

---
+++ Assets/02 - Scripts/01 - Terrain Brushes/MagicBrush.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MagicBrush : TerrainBrush {
+
+    public enum BrushType
+    {
+        SQUARE,
+        CIRCLE,
+        NORMAL,
+        NOISE,
+        SMOOTH
+    };
+
+    public float height = 5;
+
+    public BrushType type;
+
+    public override void draw(int x, int z) {
+        float actualHeight;
+
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+                actualHeight = terrain.get(x + xi, z + zi);
+                switch (type)
+                {
+                    case BrushType.SQUARE:
+                        terrain.set(x + xi, z + zi, height + actualHeight);
+                        break;
+
+                    case BrushType.CIRCLE:
+                        if (xi * xi + zi * zi <= radius * radius)
+                        {
+                            terrain.set(x + xi, z + zi, height + actualHeight);
+                        }
+                        break;
+
+                    case BrushType.NORMAL:
+                        float standardDeviation = radius/2;
+                        float distanceSquared = xi * xi + zi * zi;
+                        float gaussianValue = Mathf.Exp(-distanceSquared / (2 * standardDeviation * standardDeviation));
+                        float newheight = gaussianValue * height + actualHeight;
+                        terrain.set(x + xi, z + zi, (int)newheight);
+                        break;
+
+                    case BrushType.NOISE:
+                        float randomFloat = UnityEngine.Random.Range(0.0f, height) + actualHeight;
+                        terrain.set(x + xi, z + zi, (int)randomFloat);
+                        break;
+
+                    case BrushType.SMOOTH:
+                        //TO DO
+                        break;
+
+                }
+                
+            }
+        }
+    }
+}
