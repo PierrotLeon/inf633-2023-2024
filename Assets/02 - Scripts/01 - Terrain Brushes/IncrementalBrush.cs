27a93d0c864c6469b6fa0fc70dcc32367616fb54
Pierre OLLEON <pierre.olleon@gmail.com>
2023-09-25 10:55:42 +0200

    Added Erase, gaussian and smoothing brushes

--- Assets/02 - Scripts/01 - Terrain Brushes/IncrementalBrush.cs
+++ Assets/02 - Scripts/01 - Terrain Brushes/IncrementalBrush.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleBrush : TerrainBrush {
+public class IncrementalBrush : TerrainBrush {
 
-    private float height = 5;
-
+    public float height = 5;
     public override void draw(int x, int z) {
         for (int zi = -radius; zi <= radius; zi++) {
             for (int xi = -radius; xi <= radius; xi++) {
-                terrain.set(x + xi, z + zi, height);
+                float hloc = terrain.get(x + xi, z + zi);
+                terrain.set(x + xi, z + zi, hloc + height);
             }
         }
     }
---
+++ Assets/02 - Scripts/01 - Terrain Brushes/SmoothingBrush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothingBrush : TerrainBrush {
+
+
+    public override void draw(int x, int z) {
+        for (int zi = -radius; zi <= radius; zi++) {
+            for (int xi = -radius; xi <= radius; xi++) {
+                float hlocprevx = terrain.get(x + xi-1, z + zi);
+                float hlocnextx = terrain.get(x + xi+1, z + zi);
+                float hlocprevz = terrain.get(x + xi, z + zi-1);
+                float hlocnextz = terrain.get(x + xi, z + zi+1);
+                float height = (hlocprevx + hlocnextx + hlocprevz + hlocnextz) / 4;
+                terrain.set(x + xi, z + zi, height);
+            }
+        }
+    }
+}
